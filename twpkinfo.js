5ec2c5efffe1446c8288fe4cdc61e6d05facce37
sa411022 <sa411022@gmail.com>
2026-07-05 16:40:36 +0800

    Update twpkinfo.js

--- twpkinfo.js
+++ twpkinfo.js
@@ -1,1 +1,6 @@
-document.body.querySelector("div.fc-monetization-dialog-container").style.setProperty("display", "none", "important");
+interval = setInterval(() => {
+  if(document.body.querySelector("div.fc-monetization-dialog-container")) {
+    document.body.querySelector("div.fc-monetization-dialog-container").style.setProperty("display", "none", "important");
+    clearInterval(interval);
+  }
+}, 1000);
