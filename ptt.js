ec9754d75ecf4594fe1237e657f6ff693936d496
sa411022 <sa411022@gmail.com>
2026-07-10 15:30:34 +0800

    Update ptt.js

--- ptt.js
+++ ptt.js
@@ -2,6 +2,6 @@ document.body.querySelector("input#t").setAttribute("inputmode", "none")
 
 const link = document.createElement("link");
 link.rel = "icon";
-link.type="image/png";
+link.type = "image/png";
 link.href = chrome.runtime.getURL("ptt.png");
 document.head.appendChild(link);
