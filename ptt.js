8f887809377f8813d3770480f6e36bf4731db97e
sa411022 <sa411022@gmail.com>
2026-07-10 15:26:23 +0800

    Update ptt.js

--- ptt.js
+++ ptt.js
@@ -1,1 +1,7 @@
 document.body.querySelector("input#t").setAttribute("inputmode", "none")
+
+const link = document.createElement("link");
+link.rel = "icon";
+link.type="image/png";
+link.href = "assets/icon_128.3ffc4dd8b554423ec802560c239194fe.png";
+document.head.appendChild(link);
